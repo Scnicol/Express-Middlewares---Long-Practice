20d82bcf956578bc5a23e582909f6cd428fb6c10
Scot Nicol <scnicol@ucdavis.edu>
2022-08-06 13:54:43 -0700

    finished but not like 100% sure what is going on

--- practice-for-week-10-express-long-practice-main/server/app.js
+++ practice-for-week-10-express-long-practice-main/server/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+require('express-async-errors');
 const app = express();
+
 
 // For testing purposes, GET /
 app.get('/', (req, res) => {
   res.json("Express server running. No content provided at root level. Please use another route.");
 });
+
+
+
+// Express.json
+app.use(express.json());
 
 // For testing express.json middleware
 app.post('/test-json', (req, res, next) => {
@@ -14,10 +21,18 @@ app.post('/test-json', (req, res, next) => {
   next();
 });
 
+
+
+// make the Express.static
+app.use(express.static("assets"))
+
+
+
+
 // For testing express-async-errors
 app.get('/test-error', async (req, res) => {
   throw new Error("Hello World!")
 });
 
-const port = 5000;
+const port = 5001;
 app.listen(port, () => console.log('Server is listening on port', port));
